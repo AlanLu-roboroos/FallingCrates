e4f3f66dbcbb9a55a0c4c4aaf8e8f93d5679b0c7
alanlu <alan.lu@roboroos.org.au>
2023-10-30 18:45:57 +1030

    Set vsync to true and add q shortcut for exit

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,18 @@
 
 int main() {
   sf::RenderWindow window(sf::VideoMode(1920, 1200), "Falling Crates");
+  window.setVerticalSyncEnabled(true);
 
   while (window.isOpen()) {
     sf::Event event;
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
+      if (event.type == sf::Event::KeyPressed) {
+        if (event.key.code == sf::Keyboard::Q) {
+          window.close();
+        }
+      }
     }
 
     window.clear();
